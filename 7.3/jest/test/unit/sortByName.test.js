c64ddb97af0e8ef535308c80945b5e3cf49115ec
Netology Test <elen.1996@list.ru>
2025-07-21 22:02:39 +0300

    Solution task 1 and delete node_modules from git

--- 7.3/jest/test/unit/sortByName.test.js
+++ 7.3/jest/test/unit/sortByName.test.js
@@ -14,4 +14,21 @@ describe("Books names test suit", () => {
       "Гарри Поттер",
     ]);
   });
+  
+  it("Books names should handle descending order", () => {
+    const input = ["Волшебник изумрудного города", "Властелин Колец", "Гарри Поттер"];
+    const result = sorting.sortByName([...input]);
+    expect(result).toEqual([
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+      "Гарри Поттер",
+    ]);
+  });
+
+  it("Books names should handle equal names", () => {
+    const input = ["Гарри Поттер", "гарри поттер"];
+    const result = sorting.sortByName([...input]);
+    expect(result).toEqual(["Гарри Поттер", "гарри поттер"]);
+  });
 });
+  
